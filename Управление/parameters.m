9a5a78b5de1221e1784122827e2f9f6a3eaae4b8
Suturin Daniil <suturin.da@phystech.edu>
2025-04-27 15:07:18 +0300

    Добавил модель пропеллера и добавил константы мотора в файл с параметриами

--- Управление/parameters.m
+++ Управление/parameters.m
@@ -36,4 +36,21 @@ param.C_Lde = 0;
 param.C_Yda = 0;
 param.C_Ydr = 0;
 
+% Параметры винта (11x8 Aeronaut) и мотора (X2814)
+param.D = 11 * 25.4 / 1000; % диамер пропеллера в м
+param.CT0 = 0.12229237;
+param.CT1 = -0.05878589;
+param.CT2 = -0.10176657;
+param.CQ0 = 0.00650642;
+param.CQ1 = 0.01143496;
+param.CQ2 = -0.01978222;
+
+param.Kv = 0.010610;
+param.KQ = 0.010610;
+param.R = 3.36e-2; % Сопротивление обмотки
+param.i0 = 1.2; % Ток холостого хода
+param.BatteryVoltage = 14.8; % Номинальное напряжение батареи
+
+param.Zmotor = 0.005; % 
+
 paramInfo = Simulink.Bus.createObject(param);
